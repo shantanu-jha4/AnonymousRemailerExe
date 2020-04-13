9ae888ff8c7bf582f2e57852cf64d4f4992bc09a
Ubuntu <ubuntu@ip-172-26-3-45.ec2.internal>
2020-04-13 14:01:33 +0000

    more init

--- init.sh
+++ init.sh
@@ -3,4 +3,8 @@
 sudo apt-get install python3.7
 sudo apt-get install python3-distutils
 curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
+python3 get-pip.py --user
+echo "export PATH=~/.local/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+pip3 install pynacl --no-input
+pip3 install protobuf --no-input
