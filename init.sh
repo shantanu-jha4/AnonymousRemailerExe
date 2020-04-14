66d2d05f74cad6cf28b5948ff42e59696ccf267e
Shantanu Jha <shantanu-jha4@users.noreply.github.com>
2020-04-14 01:20:39 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install python3.7
-sudo apt-get install python3-distutils
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
-echo "export PATH=~/.local/bin:$PATH" >> ~/.bashrc
-source ~/.bashrc
+sudo apt update
+sudo apt install python3-pip
 pip3 install pynacl --no-input
 pip3 install protobuf --no-input
