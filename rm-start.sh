382e5838bfbfb6824ee4f99497b83c285a4dddae
Ubuntu <ubuntu@ip-172-26-8-3.ap-northeast-1.compute.internal>
2020-04-14 02:21:46 +0000

    remailer start script

---
+++ rm-start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt -yq install python3-pip
+pip3 install protobuf --no-input
+pip3 install pynacl --no-input
+cd /home/ubuntu/AnonymousRemailerExe/
+nohup python3 remailer.py 30999 --ts "15.223.94.116 22300" &
