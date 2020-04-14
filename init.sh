70e1bf6b886733dd3d42c7eb001cdff022b31d45
Shantanu Jha <shantanu-jha4@users.noreply.github.com>
2020-04-14 01:34:21 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,7 @@ sudo apt update
 sudo apt install python3-pip
 pip3 install pynacl --no-input
 pip3 install protobuf --no-input
+git clone https://github.com/shantanu-jha4/AnonymousRemailerExe.git -b remailer
+cd AnonymousRemailerExe
+rm remailer1_config.yaml remailer2_config.yaml remailer3_config.yaml data/remailer1.key data/remailer2.key data/remailer3.key
+mv data/remailerx.key data/remailerz.key
